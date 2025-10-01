06cd7197b87f0d0efc4ec72b73821a737dfc1d15
jasraj <jasraj@localhost.localdomain>
2025-09-30 20:48:55 -0400

    Heap free complete

--- memory-allocators/heap/heap.c
+++ memory-allocators/heap/heap.c
@@ -8,6 +8,7 @@ typedef struct {
     char* base_addr; 
     size_t curr_alloc_bytes; 
     size_t curr_alloc_chunks; 
+    size_t chunkSizes[T]; 
     char* chunkLi[T]; 
 } heap; 
 
@@ -36,11 +37,30 @@ char* my_malloc(heap *myHeap, size_t bytes) {
     myHeap->curr_alloc_bytes += bytes; 
     char* addr = myHeap->base_addr; 
     myHeap->chunkLi[myHeap->curr_alloc_chunks - 1] = addr;
+    myHeap->chunkSizes[myHeap->curr_alloc_chunks - 1] = bytes; 
     myHeap->base_addr += bytes; 
     list(myHeap); 
     return addr;
     // add 1 to chunks, add bytes to num_bytes allocated
     // return base mem addr (chunkLi[index of new thing])
+}
+
+void my_free(heap *myHeap, char *ptr) {
+    for (int i = 0; i < myHeap->curr_alloc_chunks; i++) {
+        if (myHeap->chunkLi[i] == ptr) {
+            myHeap->curr_alloc_bytes -= myHeap->chunkSizes[i]; 
+            for (int j = i; j < myHeap->curr_alloc_chunks - 1; j++) {
+                myHeap->chunkLi[i] = myHeap->chunkLi[i+1]; 
+                myHeap->chunkSizes[i] = myHeap->chunkSizes[i+1]; 
+            }
+            myHeap->chunkLi[myHeap->curr_alloc_chunks - 1] = NULL; 
+            myHeap->chunkLi[myHeap->curr_alloc_chunks - 1] = NULL; 
+            break; 
+        }
+    }
+    myHeap->curr_alloc_chunks --; 
+    
+    list(myHeap); 
 }
 
 int main() {
@@ -49,18 +69,21 @@ int main() {
     list(&myHeap); 
     char in = '!'; 
     while (in != 'q') {
-        printf("M (malloc) or Q (quit): "); 
+        printf("M (malloc) F (Free) or Q (quit): "); 
         scanf(" %c", &in); 
         if (in == 'M') {
             size_t b; 
             printf("# of bytes: "); 
             scanf(" %zu", &b); 
             my_malloc(&myHeap, b); 
-        } else if (in == 'Q') {
+        } else if (in == 'Q' || in == 'q') {
             return 0; 
+        } else if (in == 'F') {
+            printf(" Memory Address to Free: "); 
+            char *p;  scanf(" %p", &p); 
+            my_free(&myHeap, p); 
         } else {
-            printf("INVALID INPUT- EXITING PROGRAM\nExit Code 1\n");
-            return 1; 
+            printf("INVALID INPUT\n");
         }
     }
     return 0;
